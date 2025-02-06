d84562f66ce2b8f7bb91cfaf3a43592e030b2c6d
damiandrabek <damiandrabek1703@gmail.com>
2025-02-06 10:43:40 +0100

    div expanding not working properly

--- public/script.js
+++ public/script.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", function () {
   const form = document.querySelector('form[name="contact"]');
 
   form.addEventListener("submit", async function (event) {
-    
     event.preventDefault(); // Prevent the default form submission
 
     // Perform custom validation here if needed
@@ -28,26 +27,23 @@ document.addEventListener("DOMContentLoaded", function () {
       console.log("An error occurred. Please try again.");
     }
   });
-
-  const menuIcon = document.getElementById("menu-icon");
-  const navModal = document.getElementById("nav-modal");
-  const closeBtn = document.getElementById("close-btn");
-  console.log(menuIcon, navModal, closeBtn)
-
-  // Open the modal when the menu icon is clicked
-  menuIcon.addEventListener("click", function () {
-    navModal.style.width = "100%";
-  });
-
-  // Close the modal when the close button is clicked
-  closeBtn.addEventListener("click", function () {
-    navModal.style.width = "0";
-  });
-
-  // Close the modal when clicking outside the nav-content
-  navModal.addEventListener("click", function (event) {
-    if (event.target === navModal) {
-      navModal.style.width = "0";
-    }
-  });
 });
+
+const toggleExpand = (button) => {
+  const targetId = button.getAttribute("data-target");
+  const desc = document.getElementById(targetId);
+
+  if (desc.classList.contains("expandDescOut")) {
+    desc.classList.remove("expandDescOut");
+    desc.classList.add("expandDescIn");
+
+    button.classList.remove("rotateIn");
+    button.classList.add("rotateOut");
+  } else {
+    desc.classList.remove("expandDescIn");
+    desc.classList.add("expandDescOut");
+
+    button.classList.remove("rotateOut");
+    button.classList.add("rotateIn");
+  }
+};
